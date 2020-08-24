5f33f8714e123e237cf7206d184a0eedf6a7e9c5
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-08-24 22:56:01 +0530

    static pages added

--- DjangoAPI/MyApi/views.py
+++ DjangoAPI/MyApi/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Home(request):
-    return HttpResponse("<center>Hello All Present Here. Welcome to Home Page</center>")
+    return render(request, "index.html")
 
