912608d017021f40c16535f4c031197188010f7d
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-08-24 17:40:27 +0530

    ups 1

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from MyApi.views import Home
 
 
 urlpatterns = [
--- DjangoAPI/MyApi/views.py
+++ DjangoAPI/MyApi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse()
+from django.http import HttpResponse
 
 def Home(request):
     return HttpResponse("<center>Hello All Present Here. Welcome to Home Page</center>")
