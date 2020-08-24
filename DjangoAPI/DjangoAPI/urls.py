b5ff08342b05c84b0f4713524dd43e1ccef9644b
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-08-24 10:58:19 +0530

    created project

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
-def Home(request):
-    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home),
+
 ]
---
+++ DjangoAPI/MyApi/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse()
+
+def Home(request):
+    return HttpResponse("<center>Hello All Present Here. Welcome to Home Page</center>")
+
