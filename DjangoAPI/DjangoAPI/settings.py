be56f6535d14ae48c8a9e6b174a82661b5efed5a
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-08-26 13:04:57 +0530

    models created

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/MEDIA/"
+MEDIA_ROOT = BASE_DIR + "/DjangoAPI"
--- DjangoAPI/MyApi/models.py
+++ DjangoAPI/MyApi/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class MyFile(models.Model):
     image = models.ImageField()
+
+
+    #TO APPLY OR CHANGE OR ADD TO DATABASE, DO THE FOLLOWING STEPS BELOW
+
+#python manage.py makemigrations MyApi
+#python manage.py migrate
